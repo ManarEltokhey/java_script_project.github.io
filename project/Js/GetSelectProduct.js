a187f294a5d7f0a33d439ece64599368d4746783
ramiMenasa <ramimenasa@gmail.com>
2022-12-09 15:58:35 +0200

    add navbar and footer
    add navbar and footer in catigore and product

--- project/Js/GetSelectProduct.js
+++ project/Js/GetSelectProduct.js
@@ -46,7 +46,7 @@ function getProducts()
             return function (){
                  console.log(val)
                 localStorage.setItem("Pro",val)
-                window.open("http://127.0.0.1:5500/productPage.html")
+                window.open("productPage.html")
             }
 
         })(counter)
